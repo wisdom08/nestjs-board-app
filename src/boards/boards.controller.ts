b94b595008d42a959f87c029f303412fc5ef3c82
wisdom <wisdom08@kakao.com>
2022-07-24 00:20:22 +0900

    feat: 커스텀 파이프 이용해서 유효성 체크

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -4,14 +4,15 @@ import {
   Delete,
   Get,
   Param,
+  Patch,
   Post,
-  Put,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board, BoardStatus } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
@@ -38,10 +39,10 @@ export class BoardsController {
     this.boardsService.deleteBoard(id);
   }
 
-  @Put('/:id')
+  @Patch('/:id')
   updateBoardStatus(
     @Param('id') id: string,
-    @Body('status') status: BoardStatus,
+    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
   ) {
     return this.boardsService.updateBoardStatus(id, status);
   }
